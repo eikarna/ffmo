d601a16311e1103fd34c51eae8846aeef75ded25
Archenar <137246570+Archenar@users.noreply.github.com>
2023-06-21 10:50:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -47,34 +47,29 @@ def cmdb_fb(a, b, c):
     # print(tuning)
     return f"-filter:v \"tblend\" -r {a} -preset {b} -tune {tuning}"
 
+# Command Builder: Advanced
+def cmdb_adv(a, b):
+    tuning = c.split(" –")[0]
+    gr.Textbox.update(value=f"-preset {a} -tune {b}")
+    # return f"-preset {a} -tune {b}"
+
 with gr.Blocks(title="FFmo - FFmpeg Online") as main:
     with gr.Tabs():
-        with gr.TabItem("Main"):
-            with gr.Row():
-                with gr.Column() as inp_main:
-                    input_textbox = gr.Textbox(label="FFMPEG Command")
-                    input_video = gr.Video(label="Input Video")
-                with gr.Column() as out_main:
-                    output_textbox = gr.Textbox(label="Output Logs", interactive=False)
-                    output_video = gr.Video(label="Output Video", interactive=False)
-                startconv = gr.Button("Start", variant="primary").click(fn=convert, inputs=[input_video,input_textbox], outputs=[output_textbox, output_video])
-                clear_button = gr.ClearButton([input_textbox, input_video, output_textbox, output_video])
-
         with gr.TabItem("Smooth Interpolation"):
             with gr.Row():
                 with gr.Column() as inp_si:
                     input_fps = gr.Slider(1, 144, value=60, label="Frame Per Second (FPS)", info="Choose between 1 and 144 Fps")
                     input_preset = gr.Dropdown(["ultrafast", "superfast", "veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow"], value=["veryslow"], label="Preset (Required)", info="Semakin lama (slow), semakin bagus hasilnya.")
                     input_tune = gr.Radio(["film – use for high quality movie content; lowers deblocking", "animation – good for cartoons; uses higher deblocking and more reference frames", "grain – preserves the grain structure in old, grainy film material", "stillimage – good for slideshow-like content", "fastdecode – allows faster decoding by disabling certain filters", "zerolatency – good for fast encoding and low-latency streaming", "psnr – ignore this as it is only used for codec development", "ssim – ignore this as it is only used for codec development"], value=["film – use for high quality movie content; lowers deblocking"], label="Tune (Required)", info="Tuning Setting")
-                    input_video2 = gr.Video(label="Input Video")
-                    input_textbox2 = gr.Textbox(label="FFMPEG Command")
+                    input_video = gr.Video(label="Input Video")
+                    input_textbox = gr.Textbox(label="FFMPEG Command")
 
                 with gr.Column() as out_si:
-                    output_textbox2 = gr.Textbox(label="Output Logs", interactive=False)
-                    output_video2 = gr.Video(label="Output Video", interactive=False)
-                    buildcmd = gr.Button("Build FFMPEG Command", variant="primary").click(fn=cmdb_si, inputs=[input_fps,input_preset,input_tune], outputs=[input_textbox2])
-                startconv2 = gr.Button("Start", variant="primary").click(fn=convert, inputs=[input_video2,input_textbox2], outputs=[output_textbox2, output_video2])
-                clear_button2 = gr.ClearButton([input_fps, input_preset, input_tune, input_video2, input_textbox2, output_textbox2, output_video2])
+                    output_textbox = gr.Textbox(label="Output Logs", interactive=False)
+                    output_video = gr.Video(label="Output Video", interactive=False)
+                    buildcmd = gr.Button("Build FFMPEG Command", variant="primary").click(fn=cmdb_si, inputs=[input_fps,input_preset,input_tune], outputs=[input_textbox])
+                startconv = gr.Button("Start", variant="primary").click(fn=convert, inputs=[input_video,input_textbox], outputs=[output_textbox, output_video])
+                clear_button = gr.ClearButton([input_fps, input_preset, input_tune, input_video, input_textbox, output_textbox, output_video])
         
         with gr.TabItem("Frame Blending"):
             with gr.Row():
@@ -82,15 +77,31 @@ with gr.Blocks(title="FFmo - FFmpeg Online") as main:
                     input_fps2 = gr.Slider(1, 144, value=60, label="Frame Per Second (FPS)", info="Choose between 1 and 144 Fps")
                     input_preset2 = gr.Dropdown(["ultrafast", "superfast", "veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow"], value=["veryslow"], label="Preset (Required)", info="Semakin lama (slow), semakin bagus hasilnya.")
                     input_tune2 = gr.Radio(["film – use for high quality movie content; lowers deblocking", "animation – good for cartoons; uses higher deblocking and more reference frames", "grain – preserves the grain structure in old, grainy film material", "stillimage – good for slideshow-like content", "fastdecode – allows faster decoding by disabling certain filters", "zerolatency – good for fast encoding and low-latency streaming", "psnr – ignore this as it is only used for codec development", "ssim – ignore this as it is only used for codec development"], value=["film – use for high quality movie content; lowers deblocking"], label="Tune (Required)", info="Tuning Setting")
-                    input_video3 = gr.Video(label="Input Video")
-                    input_textbox3 = gr.Textbox(label="FFMPEG Command")
+                    input_video2 = gr.Video(label="Input Video")
+                    input_textbox2 = gr.Textbox(label="FFMPEG Command")
 
                 with gr.Column() as out_fb:
+                    output_textbox2 = gr.Textbox(label="Output Logs", interactive=False)
+                    output_video2 = gr.Video(label="Output Video", interactive=False)
+                    buildcmd2 = gr.Button("Build FFMPEG Command", variant="primary").click(fn=cmdb_fb, inputs=[input_fps2,input_preset2,input_tune2], outputs=[input_textbox2])
+                startconv2 = gr.Button("Start", variant="primary").click(fn=convert, inputs=[input_video2,input_textbox2], outputs=[output_textbox2, output_video2])
+                clear_button2 = gr.ClearButton([input_fps2, input_preset2, input_tune2, input_video2, input_textbox2, output_textbox2, output_video2])
+
+        with gr.TabItem("Advanced"):
+            with gr.Row():
+                with gr.Column() as inp_main:
+                    input_preset3 = gr.Dropdown(["ultrafast", "superfast", "veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow"], value=["veryslow"], label="Preset (Required)", info="Semakin lama (slow), semakin bagus hasilnya.")
+                    input_tune3 = gr.Radio(["film – use for high quality movie content; lowers deblocking", "animation – good for cartoons; uses higher deblocking and more reference frames", "grain – preserves the grain structure in old, grainy film material", "stillimage – good for slideshow-like content", "fastdecode – allows faster decoding by disabling certain filters", "zerolatency – good for fast encoding and low-latency streaming", "psnr – ignore this as it is only used for codec development", "ssim – ignore this as it is only used for codec development"], value=["film – use for high quality movie content; lowers deblocking"], label="Tune (Required)", info="Tuning Setting")
+                    input_textbox3 = gr.Textbox(label="FFMPEG Command")
+                    input_video3 = gr.Video(label="Input Video")
+                    input_preset3.change(fn=cmdb_adv, inputs=[input_preset3, input_tune3], outputs=input_textbox3)
+                    input_tune3.change(fn=cmdb_adv, inputs=[input_preset3, input_tune3], outputs=input_textbox3)
+          
+                with gr.Column() as out_main:
                     output_textbox3 = gr.Textbox(label="Output Logs", interactive=False)
                     output_video3 = gr.Video(label="Output Video", interactive=False)
-                    buildcmd2 = gr.Button("Build FFMPEG Command", variant="primary").click(fn=cmdb_fb, inputs=[input_fps2,input_preset2,input_tune2], outputs=[input_textbox3])
                 startconv3 = gr.Button("Start", variant="primary").click(fn=convert, inputs=[input_video3,input_textbox3], outputs=[output_textbox3, output_video3])
-                clear_button3 = gr.ClearButton([input_fps2, input_preset2, input_tune2, input_video3, input_textbox3, output_textbox3, output_video3])
+                clear_button3 = gr.ClearButton([input_textbox3, input_video3, output_textbox3, output_video3])
 
 # Launch the combined interface
 if __name__ == "__main__":
